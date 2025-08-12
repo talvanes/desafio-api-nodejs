af34a9ccd992fadba9bcc71e952acfc960f15b11
Talvanes Ferreira de Sousa <contato@talvanes.dev>
2025-08-12 12:38:26 -0300

    feat: initialize project with TypeScript, Fastify, and basic course API
    - Add package.json and package-lock.json with dependencies
    - Create server.ts for Fastify server and course routes
    - Add requests.rest for API testing
    - Include tsconfig.json for TypeScript configuration
    - Add .gitignore to exclude node_modules

---
+++ server.ts
@@ -0,0 +1,53 @@
+import fastify from "fastify";
+import { randomUUID } from "node:crypto";
+
+const server = fastify();
+
+const courses = [
+  { id: "1", title: "Responsive Web Design (RWD) Course" },
+  { id: "2", title: "Vanilla JS Course" },
+  { id: "3", title: "React.js Course" },
+];
+
+type Params = {
+  id: string
+}
+
+type Body = {
+  title: string
+}
+
+server.get("/courses", (_, reply) => {
+  return reply.send({ courses });
+});
+
+server.get("/courses/:id", (request, reply) => {
+  const { id } = request.params as Params;
+
+  const course = courses.find((course) => course.id === id);
+  if (course) {
+    return reply.send({ course });
+  }
+
+  return reply.status(404).send();
+});
+
+server.post("/courses", (request, reply) => {
+  const courseId = randomUUID();
+  const { title: courseTitle } = request.body as Body;
+
+  if (!courseTitle) {
+    return reply.status(400).send({ message: "Course title is mandatory!" });
+  }
+
+  courses.push({
+    id: courseId,
+    title: courseTitle,
+  });
+
+  return reply.status(201).send({ courseId });
+});
+
+server.listen({ port: 3333 }).then(() => {
+  console.log("HTTP server running!");
+});
