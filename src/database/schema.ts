c41c0edb586ee887b6fb424538eb6108bdb635a6
Talvanes Ferreira de Sousa <contato@talvanes.dev>
2025-08-14 12:44:29 -0300

    feat: add enrollments table and seed data for users and courses

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,4 +1,4 @@
-import {pgTable, text, uuid} from 'drizzle-orm/pg-core'
+import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core'
 
 export const usersTable = pgTable('users', {
     id: uuid().primaryKey().defaultRandom(),
@@ -10,4 +10,11 @@ export const coursesTable = pgTable('courses', {
     id: uuid().primaryKey().defaultRandom(),
     title: text().notNull().unique(),
     description: text(),
+})
+
+export const enrollmentsTable = pgTable('enrollments', {
+    id: uuid().primaryKey().defaultRandom(),
+    userId: uuid().notNull().references(() => usersTable.id),
+    courseId: uuid().notNull().references(() => coursesTable.id),
+    createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
 })
---
+++ src/database/seed.ts
@@ -0,0 +1,24 @@
+import { fakerPT_BR as faker } from "@faker-js/faker"
+import { db } from "./client.ts"
+import { coursesTable, enrollmentsTable, usersTable } from "./schema.ts"
+
+async function seed() {
+    const insertedUsers = await db.insert(usersTable).values([
+        { name: faker.person.fullName(), email: faker.internet.email() },
+        { name: faker.person.fullName(), email: faker.internet.email() },
+        { name: faker.person.fullName(), email: faker.internet.email() },
+    ]).returning()
+
+    const insertedCourses = await db.insert(coursesTable).values([
+        { title: faker.lorem.words(4) },
+        { title: faker.lorem.words(4) },
+    ]).returning()
+
+    await db.insert(enrollmentsTable).values([
+        { courseId: insertedCourses[0].id, userId: insertedUsers[0].id },
+        { courseId: insertedCourses[0].id, userId: insertedUsers[1].id },
+        { courseId: insertedCourses[1].id, userId: insertedUsers[2].id },
+    ])
+}
+
+seed()
